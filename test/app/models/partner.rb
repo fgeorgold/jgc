34770463ce6daefcc13ff02673fd0c8045d868a5
Nipun <nipunb@stanford.edu>
2009-05-20 15:08:49 -0700

    Cleaning the repository

--- test/app/models/partner.rb
+++
@@ -1,8 +0,0 @@
-class Partner < ActiveRecord::Base
-  belongs_to :organizations
-  attr_accessor :should_destroy
-  
-  def should_destroy?
-    should_destroy.to_i == 1
-  end
-end
--- test/db/migrate/20090308010800_add_affiliate_organization.rb
+++
@@ -1,8 +0,0 @@
-class AddAffiliateOrganization < ActiveRecord::Migration
-  def self.up
-    add_column :users, :affiliateOrg,  :string
-  end
-
-  def self.down
-  end
-end
