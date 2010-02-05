71cceb4242988c34fccaecdaa8a93c1361aa6287
Jeffrey Wong <jeff.ct.wong@gmail.com>
2010-02-05 03:12:33 -0800

    Fixing rake bugs before Hackathon

--- db/migrate/20090821201002_change_programsin_activity.rb
+++
@@ -1,9 +0,0 @@
-class ChangeProgramsinActivity < ActiveRecord::Migration
-  def self.up
-	rename_column :activities, :programs, :otherPrograms    
-  end
-
-  def self.down
-	rename_column :activities, :otherPrograms, :programs
-  end
-end
